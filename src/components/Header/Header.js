85aa0f896a2160f277ead79c83f58173e15e7153
marikamulder <113202631+marikamulder@users.noreply.github.com>
2023-02-14 10:32:09 -0500

    created outline, header, and footer to start

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <div>Book finder App</div>
+        </header>
+    );
+}
+
+export default Header;
